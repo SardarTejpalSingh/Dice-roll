5b684b287c1acfd795a4dd02232c893b6a661b34
SardarTejpalSingh <sardartejpal@gmail.com>
2020-06-10 13:59:16 +0800

    Completed the website just basic on javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+var rand1 = Math.floor(Math.random()*6 + 1);
+var s = document.querySelector(".img1");
+s.setAttribute("src", "images/dice"+rand1+".png");
+
+var rand2 = Math.floor(Math.random()*6 + 1);
+document.querySelector(".img2").setAttribute("src", "images/dice"+rand2+".png");
+
+if(rand1 > rand2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+}
+else if(rand2 > rand1)
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+else
+    document.querySelector("h1").textContent = "Draw!";
